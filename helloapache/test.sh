a5dba992bd5236c5e578dae09554c4937e9f292d
Tomas Kral <tkral@redhat.com>
2015-11-05 11:47:36 +0100

    test

--- helloapache/test.sh
+++ helloapache/test.sh
@@ -5,6 +5,9 @@ IMGNAME="helloapache"
 docker build -t $IMGNAME helloapache/
 atomic run $IMGNAME --provider=docker
 
+docker ps
+ls  /var/lib/atomicapp
+
 curl localhost:80
 
 atomic stop $IMGNAME --provider=docker
